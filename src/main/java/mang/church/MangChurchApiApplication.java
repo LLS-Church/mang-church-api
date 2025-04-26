00a2d5a4e558827f5056ff8bb16d5c7821792676
Davi Gomes <gdavi989@gmail.com>
2025-04-25 21:58:53 -0300

    feat: base structure

---
+++ src/main/java/mang/church/MangChurchApiApplication.java
@@ -0,0 +1,13 @@
+package mang.church;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangChurchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangChurchApiApplication.class, args);
+	}
+
+}
